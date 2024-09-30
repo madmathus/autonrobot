bb27a90ff7a9bd8586efadd248c6f438642168d6
Rhoy Xu <rhoyxu@gmail.com>
2024-09-29 21:39:13 -0700

    Added a java file

---
+++ source/First.java
@@ -0,0 +1,19 @@
+public class autonrobot {
+    public void moveForward() {
+        System.out.println("The robot is moving forward.");
+    }
+
+    public void turnLeft() {
+        System.out.println("The robot is turning left.");
+    }
+
+    public void stop() {
+        System.out.println("The robot has stopped.");
+    }
+
+    public static void main(String[] args) {
+        autonrobot robot = new autonrobot();
+        robot.moveForward();
+        robot.turnLeft();
+        robot.stop();
+    }
